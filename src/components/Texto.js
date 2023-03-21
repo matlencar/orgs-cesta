8d9507f8ee5e8c3ea6174337d58c86d8d15cfbfe
Mathlencar <matheus.alencar@ofsolution.com.br>
2023-03-21 11:44:58 -0300

    Refatorando a app, e criando novos componentes

--- src/components/Texto.js
+++ src/components/Texto.js
@@ -1,12 +1,22 @@
 import react from "react";
 import { StyleSheet, Text } from "react-native";
 
-export default function Texto({ children }) {
-  return <Text style={estilos.texto}>{ children }</Text>;
+export default function Texto({ children, style }) {
+    let estilo = estilos.texto;
+
+    if(style?.fontWeight === 'bold') {
+        estilo = estilos.textoNegrito;
+    }
+  return <Text style={[style, estilo]}>{ children }</Text>;
 }
 
 const estilos = StyleSheet.create({
    texto: {
     fontFamily: "MontserratRegular",
+    fontWeight: "normal",
+   },
+   textoNegrito: {
+    fontFamily: "MontserratBold",
+    fontWeight: "normal",
    } 
 })
