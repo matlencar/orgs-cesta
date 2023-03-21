624bc97f23eb4c97f0e81bf52ff93c363dd95b7d
Mathlencar <matheus.alencar@ofsolution.com.br>
2023-03-21 09:00:27 -0300

    update project orgs-cesta

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, View } from "react-native";
+import Cesta from "./src/telas/Cestas";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+const [fonteCarregada] = useFonts({
+  MontserratRegular: Montserrat_400Regular,
+  MontserratBold: Montserrat_700Bold,
+});
+
+if (!fonteCarregada) {
+   <View />;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
